478e2f75100b872c4ed8bcf4c0297f917c1e338f
angiecodinghub <angelawu625@gmail.com>
2022-03-28 19:09:30 -0500

    added docstring and cleanup.

--- src/openai_ros/task_envs/reach/reach.py
+++ src/openai_ros/task_envs/reach/reach.py
@@ -5,32 +5,48 @@ from openai_ros.robot_envs import panda_env
 import numpy as np
 import time
 
-timestep_limit_per_episode = 1000 # Can be any Value
-# register my own environment in gym.
+timestep_limit_per_episode = 1000
+
+# register environment in gym.
 register(
-        id='PandaReach-v2',
-        entry_point='openai_ros.task_envs.reach.reach:ReachEnv',
-        max_episode_steps=timestep_limit_per_episode
+        id = 'PandaReach-v2',
+        entry_point = 'openai_ros.task_envs.reach.reach:ReachEnv',
+        max_episode_steps = timestep_limit_per_episode
     )
 
 class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
     """
     This class provides all the context for the reach task we want Panda to learn. 
-    It depends on both the task and on the robot.
+    self.control_type: what kind of control to learn. "ee" or "joint". affect self.n_actions.
+    self.n_actions: number of actions.
+    self.has_object: if there's object in the environment.
+    self.block_gripper: if we want to ignore gripper.
+    self.distance_threshold: threshold distance to the goal.
+    self.reward_type: the type of reward.
+    self.init_pos: initial pose.
+    self.n_substeps: number of substeps.
+    self.gripper_extra_height: height of the gripper.
+    self.target_in_the_air: if the target is in the air.
+    self.target_offset: offset to the target.
+    self.obj_range: the range of the object.
+    self.target_range: the max distance from start to target.
+    self.action_space: the action space.
+    self.observation_space: the observation space.
+    self.initial_gripper_pos: initial gripper position.
     """
     def __init__(self, control_type="ee"): ## default control_type is ee.
         rospy.logdebug("entered Reach Env")
-        # Only variable needed to be set here
+        # set params.
         self.control_type = control_type
         self.get_params()
 
         panda_env.PandaEnv.__init__(self)
         utils.EzPickle.__init__(self)
 
-        print("reach env - setup")
-        self._env_setup(initial_qpos = self.init_pos)
+        rospy.logdebug("reach env - setup")
+        self._env_setup(self.init_pos)
  
-        print ("reach env - check obs")
+        rospy.logdebug("reach env - check obs")
         obs = self._get_obs()
         rospy.logdebug("current obs:\n")
         rospy.logdebug(obs)
@@ -43,12 +59,12 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
                 achieved_goal=spaces.Box(-10., 10., shape=obs['desired_goal'].shape, dtype=np.float32),
             )
         )
-        rospy.logdebug("Reach Env settings DONE")
+        rospy.logdebug("Reach Env init DONE")
 
 
     def get_params(self):
         """
-        Get configuration parameters.
+        get configuration parameters.
         """
         if self.control_type == "ee":
             self.n_actions = 3 # ee position
@@ -74,21 +90,23 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
         self.obj_range = 0.15
         self.target_range = 0.15
 
-    def robot_get_obs(self, data):
+    def robot_get_obs(self, joints):
         """
-        Returns all joint positions and velocities associated with a robot.
+        returns current joint positions and joint velocities of Panda.
+        :param joints: a list of joints.
+        :returns: current joint positions and joint velocities.
         """
-        if data.position is not None and data.name:
-            #names = [n for n in data.name if n.startswith('robot')]
-            names = [n for n in data.name]
+        if joints.position is not None and joints.name:
+            names = [n for n in joints.name]
             i = 0
             r = 0
+            # count the number of joints.
             for name in names:
                 r += 1
                 
             return (
-                np.array([data.position[i] for i in range(r)]),
-                np.array([data.velocity[i] for i in range(r)]),
+                np.array([joints.position[i] for i in range(r)]),
+                np.array([joints.velocity[i] for i in range(r)]),
             )
         return np.zeros(0), np.zeros(0)
 
@@ -97,18 +115,17 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
     # --------------------------------
     def _set_init_pose(self):
         """
-        Sets the Robot in its init pose
+        sets Panda in its init pose.
+        :returns: if the plan succeed.
         """
         self.gazebo.unpauseSim()
-        self.set_trajectory_joints(self.init_pos)
+        result = self.set_trajectory_joints(self.init_pos)
 
-        return True
+        return result
 
     def _init_env_variables(self):
         """
-        Inits variables needed to be initialised each time we reset at the start
-        of an episode.
-        :return:
+        init environment variables.
         """
         self.n_actions = 3
         self.has_object = False
@@ -137,8 +154,9 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
 
     def _set_action(self, action):
         """
-        Move the robot based on the action variable given.
-        ref: ee_displacement_to_target_arm_angles in panda.py.
+        move the robot based on the action variable given.
+        reference: ee_displacement_to_target_arm_angles in panda.py.
+        :param action: the action.
         """
         if self.control_type == "ee":
             assert action.shape == (3,)
@@ -148,18 +166,16 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
             current_obs = self._get_obs()
             next_obs = current_obs['observation'][:3] + ee_displacement
             next_obs[2] = max(0, next_obs[2])
-            # print("desired observation:", current_obs['desired_goal'][:3])
             rot_ctrl = [1., 0., 0., 0.] ### PLACEHOLDER FOR ORIENTATION OF EE.
             action = np.concatenate([next_obs, rot_ctrl])
             self.set_trajectory_ee(action)  
-        else: # joint angle.
+        else: # "joint"
             assert action.shape == (7,)
             action = action.copy()  # ensure action don't change
             action = np.clip(action, self.action_space.low, self.action_space.high)
             joint_displacement = action * 0.05
-            current_joint, _ = self.robot_get_obs(self.joints)# current joint degree
+            current_joint, _ = self.robot_get_obs(self.joints)
             next_joint = current_joint[:7] + joint_displacement
-            self.gazebo.unpauseSim()
             # make joint 
             joint_degree = {
             'panda_joint1': float(next_joint[0]),
@@ -169,27 +185,20 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
             'panda_joint5': float(next_joint[4]),
             'panda_joint6': float(next_joint[5]),
             'panda_joint7': float(next_joint[6]),
-        }
+            }
             self.set_trajectory_joints(joint_degree)
     
      
     def _get_obs(self):
         """
-        Here we define what sensor data do we want our robot to know.
+        get the observation of the state now.
+        :returns: observation (ee position and velocity), achieved goal and desired goal.
         """
-        # TODO
-        grip_pos = self.get_ee_pose() # pose
+        grip_pos = self.get_ee_pose()
         grip_pos_array = np.array([grip_pos.pose.position.x, grip_pos.pose.position.y, grip_pos.pose.position.z])
         grip_vel_array = np.array([0., 0., 0.])
-        #robot_qpos, robot_qvel = self.robot_get_obs(self.joints) 
-        # gripper's velocity
-        ##### EE VELOCITY IS NOT ACCESSIBLE IN MOVEIT ########
-        #gripper_state = robot_qpos[-1:] # -2, not a good fix tho.
-        #gripper_vel = robot_qvel[-2:] #* dt  # change to a scalar if the gripper is made symmetric
-        #print("got ee pose array gggg", gripper_vel) 
         achieved_goal = self._sample_achieved_goal(grip_pos_array)
         obs = np.concatenate([
-        #    grip_pos_array, gripper_state, gripper_vel,
             grip_pos_array, grip_vel_array,
         ])
 
@@ -201,7 +210,9 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
 
     def _is_done(self, observations):
         """
-        Decide if episode is done based on the observations
+        check if the episode and is done.
+        :param observations: the observations of the state.
+        :returns: if the episode is done.
         """
         if np.linalg.norm(observations['achieved_goal'] - observations['desired_goal'], axis = -1) < self.distance_threshold:
             return True
@@ -210,14 +221,16 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
 
     def _compute_reward(self, observations, done):
         """
-        Return the reward based on the observations given
+        return the reward based on the observations given.
+        :param observations: the observations of the state.
+        :param done: if the episode is done.
+        :returns: the reward.
         """
-        # TODO
         if done:
             return -0
         distance = np.linalg.norm(observations['achieved_goal'] - observations['desired_goal'], axis = -1)
         if self.reward_type == "sparse":
-            return -np.array(distance > self.distance_threshold, dtype=np.float64)
+            return -np.array(distance > self.distance_threshold, dtype = np.float64)
         else:
             return -distance
     # --------------------------------
@@ -225,33 +238,34 @@ class ReachEnv(panda_env.PandaEnv, utils.EzPickle):
     # internal methods for ReachEnv.
     # -----------------------------
     def _sample_achieved_goal(self, grip_pos_array):
+        """
+        return the achieved goal.
+        :param grip_pos_array: the position array of the gripper.
+        :returns: the achieved goal.
+        """
         achieved_goal = grip_pos_array.copy()
         return achieved_goal
 
     def _sample_goal(self):
-        goal = self.initial_gripper_xpos[:3] + self.np_random.uniform(-0.15, 0.15, size=3)
+        """
+        sample a goal according to self.target_range.
+        :returns: the goal.
+        """
+        goal = self.initial_gripper_pos[:3] + self.np_random.uniform(-self.target_range, self.target_range, size = 3)
         return goal
 
-    def _env_setup(self, initial_qpos):
-        print("Desired Init Pos:", initial_qpos)
+    def _env_setup(self, initial_pos):
+        """
+        setup the init pose of the robot and the gripper, and set a goal.
+        :param initial_pos: initial pose of the joints.
+        """
         self.gazebo.unpauseSim()
-        start = time.time()
-        self.set_trajectory_joints(initial_qpos) ###### THIS SHOULD SET THE ARM IN THE DESIRED POSITION, SUCCEED.
-        end = time.time()
-        print("env setup execution time:", end - start)
+        self.set_trajectory_joints(initial_pos)
 
-        # Move gripper into position.
-        # gripper_target = np.array([0.498, 0.005, 0.431 + self.gripper_extra_height])# + self.sim.data.get_site_xpos('robot0:grip')
-        # gripper_rotation = np.array([1., 0., 1., 0.])
-        # action = np.concatenate([gripper_target, gripper_rotation])
-        # self.set_trajectory_ee(action)
-
-        # Extract information for sampling goals.
+        # get the position of the end effector.
         gripper_pos = self.get_ee_pose()
         gripper_pose_array = np.array([gripper_pos.pose.position.x, gripper_pos.pose.position.y, gripper_pos.pose.position.z])
-        self.initial_gripper_xpos = gripper_pose_array.copy()
+        self.initial_gripper_pos = gripper_pose_array.copy()
             
         self.goal = self._sample_goal()
-        self._get_obs()
-    
     # -----------------------------
